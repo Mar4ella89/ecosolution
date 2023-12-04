b80605f586dbd040e4629e5ffc612f89e7d4464f
Marat Baikin <3371335@gmail.com>
2023-12-04 23:49:29 +0200

    complex fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import Header from 'components/Header/Header';
 
 import style from 'App.module.scss';
-import LogoIcon from 'images/icons/LogoIcon';
-import RightIcon from 'images/icons/RightIcon';
-// import BurgerIcon from 'images/icons/BurgerIcon';
-// import DownIcon from 'images/icons/DownIcon';
-// import Contacts from 'components/Contacts/Contacts';
+
 import Main from 'components/Main/Main';
 import About from 'components/About/About';
 import Electricity from 'components/Electricity/Electricity';
 import Cases from 'components/Cases/Cases';
 import FAQ from 'components/FAQ/FAQ';
 import ContactsUs from 'components/ContactUs/ContactUs';
+import Footer from 'components/Footer/Footer';
 
 import Container from 'components/Container/Container';
 
@@ -29,16 +26,8 @@ function App() {
           <ContactsUs />
         </main>
         <footer>
-          <a href="index.html">
-            <LogoIcon color={'#173D33'} className={style.logoIcon} />
-          </a>
-          <p>79005, Ukraine, Lvivstreet. Shota Rustaveli, 7</p>
-          <img />
-          <p>office@ecosolution.com</p>
-          <button>UP</button>
-          <p>ecosolution © 2023</p>
-        </footer>{' '}
-        */
+          <Footer />
+        </footer>
       </Container>
     </>
   );
