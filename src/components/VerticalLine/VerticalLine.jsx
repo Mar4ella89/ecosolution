b2b5d8d6ddad898a5bcce1455b6d94fad5071cc3
Marat Baikin <3371335@gmail.com>
2023-12-09 10:37:46 +0200

    reloading app

--- src/components/VerticalLine/VerticalLine.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import style from './VerticalLine.module.scss';
-
-const VerticalLine = () => {
-  return <div className={style.divLineV}></div>;
-};
-export default VerticalLine;
