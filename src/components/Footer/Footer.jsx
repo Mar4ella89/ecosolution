840ecf5dfddf1549df867c19fdb0ea1960e7ac4c
Marat Baikin <3371335@gmail.com>
2023-12-12 17:48:46 +0200

    fix Footer style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import HorizontLine from 'components/HorizontLine/HorizontLine';
 import LogoIcon from 'images/icons/LogoIcon';
 import Contacts from 'components/Contacts/Contacts';
 import UpIcon from 'images/icons/UpIcon';
@@ -7,16 +8,21 @@ import UpIcon from 'images/icons/UpIcon';
 import style from './Footer.module.scss';
 
 const Footer = () => {
-
   return (
-    <>
-      <a href="index.html">
-        <LogoIcon color={'#173D33'} className={style.logoIcon} />
-      </a>
-      <UpIcon className={style.upIcon}/>
-      <Contacts/>
+    <div className={style.footer}>
+      <HorizontLine />
+      <div className={style.logowrapper}>
+        <a href="index.html">
+          <LogoIcon color={'#173D33'} className={style.logoIcon} />
+        </a>
+        <button type="button" className={style.upBtn}>
+          <UpIcon />
+        </button>
+      </div>
+
+      <Contacts />
       <p>ecosolution © 2023</p>
-    </>
+    </div>
   );
 };
 
