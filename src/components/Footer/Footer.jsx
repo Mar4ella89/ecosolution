9db79db20f77bb86752fcc854273e1772edfd7bb
Marat Baikin <3371335@gmail.com>
2023-12-20 18:01:30 +0200

    start fix footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,6 +7,9 @@ import LogoIcon from 'images/icons/LogoIcon';
 import Contacts from 'components/Contacts/Contacts';
 import UpIcon from 'images/icons/UpIcon';
 
+import { ReactComponent as FbIcon } from 'images/svg/fb.svg';
+import { ReactComponent as InstIcon } from 'images/svg/inst.svg';
+
 import style from './Footer.module.scss';
 
 const Footer = () => {
@@ -22,6 +25,18 @@ const Footer = () => {
             <UpIcon />
           </button>
         </div>
+        <ul className={style.socialList}>
+          <li>
+            <a href="https://facebook.com/" className={style.link}>
+              <FbIcon className={style.social} />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/" className={style.link}>
+              <InstIcon className={style.social} />
+            </a>
+          </li>
+        </ul>
 
         <Contacts />
         <p>ecosolution © 2023</p>
