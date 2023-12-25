61177917e13693ff9c828961f1c917a810ccf40a
Marat Baikin <3371335@gmail.com>
2023-12-25 16:42:46 +0200

    updated footer styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import useWindowWidth from 'hooks/useWindowWidth';
+
 import HorizontLine from 'components/HorizontLine/HorizontLine';
 import Container from 'components/Container/Container';
 import Social from 'components/Social/Social';
@@ -10,19 +12,21 @@ import UpIcon from 'images/icons/UpIcon';
 import style from './Footer.module.scss';
 
 const Footer = () => {
+  const width = useWindowWidth();
   return (
     <Container>
       <div className={style.footer}>
         <HorizontLine />
         <div className={style.logowrapper}>
-          <a href="index.html">
+          <a href="index.html" className={style.link}>
             <LogoIcon color={'#173D33'} className={style.logoIcon} />
           </a>
+          {width > 768 && <Social customStyles={style.socialList} />}
           <button type="button" className={style.upBtn}>
             <UpIcon />
           </button>
         </div>
-        <Social customStyles={style.socialList} />
+        {width < 768 && <Social customStyles={style.socialList} />}
         <div className={style.contactWrapper}>
           <p className={style.contact}>
             <span className={style.address}>
